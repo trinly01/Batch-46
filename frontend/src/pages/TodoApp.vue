e51b4b4cafe2da4ff5aae6c9be9deb00c9180ede
Trinmar Boado <timi.boado@gmail.com>
2022-11-06 15:50:52 +0800

    q-input

--- frontend/src/pages/TodoApp.vue
+++ frontend/src/pages/TodoApp.vue
@@ -1,15 +1,15 @@
 <template>
   <q-toolbar class="bg-primary text-white">
-      <q-btn flat round dense icon="menu" class="q-mr-sm" />
-      <q-avatar>
-        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-      </q-avatar>
+    <q-btn flat round dense icon="menu" class="q-mr-sm" />
+    <q-avatar>
+      <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+    </q-avatar>
 
-      <q-toolbar-title>Batch 46</q-toolbar-title>
+    <q-toolbar-title>Batch 46</q-toolbar-title>
 
-      <q-btn flat round dense icon="whatshot" />
-    </q-toolbar>
-  <input type="text" />
+    <q-btn flat round dense icon="whatshot" />
+  </q-toolbar>
+  <q-input outlined modelValue="text" label="Outlined" />
   <ul>
     <li>zxcsfsdfs dsdf sdf d</li>
     <li>zxcsfsdfs dsdf sdf d</li>
