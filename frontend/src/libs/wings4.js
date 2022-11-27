3160b87321e29a2e5abd1c817ede5e29c85668bf
Trinmar Boado <timi.boado@gmail.com>
2022-11-27 15:45:11 +0800

    generate authenticated service

--- frontend/src/libs/wings4.js
+++ frontend/src/libs/wings4.js
@@ -1,5 +1,17 @@
+import store from 'src/store/store.js'
 import wings from 'wings4'
 
 const app = wings('http://localhost:3030')
+
+app.on('login', (user) => {
+  console.log('Logged in', user)
+  store.user = user.user
+})
+
+app.authenticate()
+
+app.on('logout', (user) => {
+  store.user = null
+})
 
 export default app
